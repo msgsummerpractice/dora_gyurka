50ddfe5ffec0d8f5a2ff17078f3ffc4bd9364cb2
Gyurka Dora <dora.gyurka25@gmail.com>
2026-07-24 09:13:39 +0200

    Complete change request

--- spring-project/src/main/java/com/example/App.java
+++ spring-project/src/main/java/com/example/App.java
@@ -1,25 +1,20 @@
 package com.example;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-/**
- * Hello world!
- *
- */
 
 @Configuration
 @ComponentScan(basePackages = "com.example")
-public class App 
-{
-    
-    
-    public static void main( String[] args )
-    {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //Intern intern = context.getBean("intern", Intern.class);
-        //intern.display();
-     
+public class App {
+
+    public static void main(String[] args) {
+        // ApplicationContext context = new
+        // ClassPathXmlApplicationContext("applicationContext.xml");
+        // Intern intern = context.getBean("intern", Intern.class);
+        // intern.display();
+
         ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         Intern intern = context.getBean(Intern.class);
         intern.display();
