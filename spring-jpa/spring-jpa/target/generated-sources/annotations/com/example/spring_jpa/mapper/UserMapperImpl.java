e31f0eeb452f411db85e65eee1a7cd359cb62eb9
Gyurka Dora <dora.gyurka25@gmail.com>
2026-07-28 14:44:37 +0200

    Finish rest

--- spring-jpa/spring-jpa/target/generated-sources/annotations/com/example/spring_jpa/mapper/UserMapperImpl.java
+++ spring-jpa/spring-jpa/target/generated-sources/annotations/com/example/spring_jpa/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-28T12:45:00+0200",
+    date = "2026-07-28T14:43:04+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.3 (Eclipse Adoptium)"
 )
 @Component
