7d6a80710ee1aa87672bd671a5234e52ce212b01
Gyurka Dora <dora.gyurka25@gmail.com>
2026-07-29 15:55:49 +0200

    Angular-day1

--- frontend/Angular/angular-project/src/app/app.ts
+++ frontend/Angular/angular-project/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = signal('angular-project');
-}
+export class App {}
